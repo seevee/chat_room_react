096a5f92a145537a49c3b68f073cee165bd89f7d
SeeVee <cmichaelvincent@gmail.com>
2016-09-22 15:13:04 +0200

    + wired up local storage

--- src/index.js
+++ src/index.js
@@ -2,16 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
-import ReduxPromise from 'redux-promise'
+//import ReduxPromise from 'redux-promise'
+import * as storage from 'redux-storage'
+import createEngine from 'redux-storage-engine-localstorage'
 
 import './styles/style.scss'
 import App from './components/app'
 import reducers from './reducers'
 
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore)
+const reducer = storage.reducer(reducers)
+const engine = createEngine('my-save-key')
+const localStorageMiddleware = storage.createMiddleware(engine)
+const createStoreWithMiddleware = applyMiddleware(localStorageMiddleware)(createStore)
+const store = createStoreWithMiddleware(reducer)
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'))
+const load = storage.createLoader(engine)
+load(store)
+  .then((newState) => console.log('Loaded state: ', newState))
+  .catch(() => console.log('Failed to load previous state'))
+
+  ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducer)}>
+      <App />
+    </Provider>
+    , document.querySelector('.container'))
