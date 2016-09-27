fbbb52eaaea0e0771c7389d871dc847313e0eb34
seevee <cmichaelvincent@gmail.com>
2016-09-27 17:55:42 -0600

    + updated icon styling, removed generated styles from repo

--- src/components/user.js
+++ src/components/user.js
@@ -11,26 +11,53 @@ class User extends Component {
     user.status = Math.random()
     this.props.updateUser(user)
   }
+  styleIndicator() {
+    let indicatorColor = '#43A6D8'
+    let spin = true
+    switch (this.props.user.status) {
+      case 'Online':
+        indicatorColor = 'green'
+        spin = true
+        break
+      case 'Offline':
+        indicatorColor = '#666666'
+        spin = true
+        break
+      case 'Away':
+        indicatorColor = 'yellow'
+        spin = false
+        break
+    }
+    return {
+      color: indicatorColor,
+      spin: spin
+    }
+  }
   render() {
+    let indicatorStyle = this.styleIndicator()
+    let style = {
+      color: indicatorStyle.color
+    }
+
     return (
       <div className='user'>
-        <i className='user-icon fa fa-circle-o-notch fa-2x' />
-        <Avatar size='50' className='user-avatar' />
-        <div className='user-info'>
-          <div className='user-name'>{this.props.user.username}</div>
-          <div
-            className='user-status'
-            onClick={this.onStatusClick.bind(this)}>
-            {this.props.user.status}
+        <i style={style} className={(indicatorStyle.spin ? 'fa-spin ' : '') + 'user-icon fa fa-circle-o-notch fa-2x'} />
+          <Avatar size='50' className='user-avatar' name={this.props.user.username} />
+          <div className='user-info'>
+            <div className='user-name'>{this.props.user.username}</div>
+            <div
+              className='user-status'
+              onClick={this.onStatusClick.bind(this)}>
+              {this.props.user.status}
+            </div>
           </div>
         </div>
-      </div>
     )
+        }
   }
-}
 
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ updateUser }, dispatch)
-}
+  function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ updateUser }, dispatch)
+  }
 
-export default connect(null, mapDispatchToProps)(User)
+  export default connect(null, mapDispatchToProps)(User)
