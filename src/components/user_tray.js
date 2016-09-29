181df77d91b9cf7a46d22c5982952c54cd1e125f
seevee <cmichaelvincent@gmail.com>
2016-09-28 23:48:21 -0600

    - removed unnecessary dependencies

--- src/components/user_tray.js
+++ src/components/user_tray.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { GenericScrollBox, FastTrack } from 'react-scroll-box'
 import User from './user'
 
 class UserTray extends Component {
@@ -15,11 +14,11 @@ class UserTray extends Component {
 
   render() {
     return (
-      <GenericScrollBox scrollableY fastTrack={FastTrack.PAGING} className='user-scroll'>
-        <div className='scroll-box__viewport user-tray'>
+      <div className='user-scroll'>
+        <div className='user-tray'>
           {this.renderUsers()}
         </div>
-      </GenericScrollBox>
+      </div>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
-//import ReduxPromise from 'redux-promise'
 import * as storage from 'redux-storage'
 import createEngine from 'redux-storage-engine-localstorage'
 
