389089f61f2c13a418fae2b47cdcb3c62c118458
seevee <cmichaelvincent@gmail.com>
2016-09-28 23:21:59 -0600

    + added randomized status updates, expanded game list

--- src/components/new_chat_form.js
+++ src/components/new_chat_form.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { reset, reduxForm, Field } from 'redux-form'
 import { connect } from 'react-redux'
-import { createChat } from '../actions/index'
+import { createChat, updateUser } from '../actions/index'
 import * as config from '../config'
 
 class NewChatForm extends Component {
@@ -11,7 +11,9 @@ class NewChatForm extends Component {
     this.onSend = this.onSend.bind(this)
     this.sendBotReply = this.sendBotReply.bind(this)
     this.recurseRandomMessages = this.recurseRandomMessages.bind(this)
+    this.recurseRandomStatuses = this.recurseRandomStatuses.bind(this)
     this.recurseRandomMessages()
+    this.recurseRandomStatuses()
   }
 
   onSend(props) {
@@ -53,6 +55,18 @@ class NewChatForm extends Component {
       this.props.dispatch(createChat(message))
       this.recurseRandomMessages()
     }, config.randomTime() * 3)
+  }
+
+  recurseRandomStatuses() {
+    let users = this.props.users.users.filter((user) => {
+      return user.id !== 1
+    })
+    let user = users[Math.floor(Math.random() * users.length)]
+    user.status = config.randomStatus()
+    setTimeout(() => {
+      this.props.dispatch(updateUser(user))
+      this.recurseRandomStatuses()
+    }, config.randomTime() * 8)
   }
 
   render() {
--- src/constants/games.js
+++ src/constants/games.js
@@ -1,8 +1,18 @@
 const games = [
   'Halo',
   'Battlefront',
-  'Civilization',
+  'Sid Meier\'s Civilization V',
   'The Witcher',
-  'Battlefield One'
+  'Battlefield 1',
+  'Battlefield 4',
+  'Duskers',
+  'Kerbal Space Program',
+  'Half Life 3',
+  'DOOM',
+  'Counter-Strike: Global Offensive',
+  'Portal 2',
+  'Metro 2033',
+  'Deus Ex',
+  'Dragon Age'
 ]
 export default games
