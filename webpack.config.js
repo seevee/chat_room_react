f888759e0640fda3f0db2d276b1624a861646bb8
Chris <cmichaelvincent@gmail.com>
2018-01-26 14:47:44 -0700

    updated dependencies, fixed webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const autoprefixer = require('autoprefixer');
 const webpack = require('webpack');
 
 module.exports = {
@@ -12,21 +11,26 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
-    loaders: [{
+    rules: [{
       test: /\.js$/,
       exclude: /node_modules/,
-      loader: 'babel',
+      loader: 'babel-loader',
       query: {
         presets: ['react', 'es2015', 'stage-1']
       }
     },
     {
       test: /\.scss$/,
-      loader: ExtractTextPlugin.extract("style-loader", "css?sourceMap!resolve-url!postcss-loader!sass?sourceMap")
+      loaders: ['style-loader', 'css-loader', 'resolve-url-loader', 'sass-loader?sourceMap']
+      //use: ExtractTextPlugin.extract("style-loader", "css?sourceMap!resolve-url!postcss-loader!sass?sourceMap")
     },
     {
       test: /\.woff(2)?(\?[a-z0-9]+)?$/,
-      loader: "url-loader?limit=10000&mimetype=application/font-woff"
+      loader: 'url-loader',
+      options: {
+        limit: 10000,
+        mimetype: 'application/font-woff'
+      }
     },
     {
       test: /\.(otf|ttf|eot|svg)(\?[a-z0-9]+)?$/,
@@ -36,7 +40,7 @@ module.exports = {
   },
   devtool: 'source-map',
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['.js', '.jsx']
   },
   devServer: {
     historyApiFallback: true,
@@ -51,6 +55,5 @@ module.exports = {
     new ExtractTextPlugin('./style.css', {
       allChunks: true
     })
-  ],
-  postcss: [autoprefixer]
+  ]
 };
